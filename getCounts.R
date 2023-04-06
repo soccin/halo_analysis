d20f8d675531d63c4ae53c8e7cae6e7af56b6d5d
Nicholas D. Socci <soccin@mskcc.org>
2023-04-06 19:16:47 -0400

    marker combo table

---
+++ getCounts.R
@@ -0,0 +1,34 @@
+source("read_halo.R")
+source("getPhenoTypes.R")
+
+oo=map(fs::dir_ls("raw",recur=T,regex="ObjectData.*.csv.gz$"),read_halo)
+di=map(oo,"dat") %>% bind_rows
+
+phenoTable=get_pheno_table(di)
+
+tbl=di %>%
+    filter(Pos==1) %>%
+    select(SID,ObjectID,Marker) %>%
+    arrange(SID,ObjectID,Marker) %>%
+    group_by(SID,ObjectID) %>%
+    summarize(PMID=paste(Marker,collapse=",")) %>%
+    ungroup %>%
+    count(PMID) %>%
+    arrange(desc(n)) %>%
+    ungroup %>%
+    left_join(phenoTable,multiple="all") %>%
+    mutate(Tag=ifelse(is.na(Tag),".NULL",Tag))
+
+totals=tbl %>%
+    group_by(PMID) %>%
+    summarize(Total=min(n)) %>%
+    arrange(desc(Total))
+
+tbl1=tbl %>%
+    spread(Tag,n) %>%
+    left_join(totals) %>%
+    arrange(desc(Total)) %>%
+    select(-Total)
+
+openxlsx::write.xlsx(tbl1,"markerComboTable.xlsx")
+
--- read_halo.R
+++ read_halo.R
@@ -1,5 +1,6 @@
 require(readr)
 require(dplyr)
+require(tidyr)
 
 read_halo<-function(hfile) {
 
@@ -13,7 +14,16 @@ read_halo<-function(hfile) {
 
 	di=di %>%
 		rename_all(~gsub(" Positive.*","",.)) %>% 
-		mutate(x0=(XMax+XMin)/2,y0=(YMax+YMin)/2)
+		mutate(x0=(XMax+XMin)/2,y0=-(YMax+YMin)/2) %>%
+		gather(Marker,Pos,all_of(markers))
+
+	if(file.exists("meta/markerRename.csv")) {
+		markerRename=read_csv("meta/markerRename.csv")
+		di=left_join(di,markerRename,by=c(Marker="Orig")) %>%
+			mutate(Marker=New) %>%
+			select(-New)
+		markers=unique(di$Marker)
+	}
 
 	list(dat=di,markers=markers)
 
